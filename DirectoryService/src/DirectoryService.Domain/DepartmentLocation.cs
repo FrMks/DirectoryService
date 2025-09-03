01baf04da5b5193eb10166a0fd7f587cc16ce9a4
FrMks <maxfrancuzov@gmail.com>
2025-09-03 18:50:59 +0300

    feat(#1659): add DepartmentLocation class

---
+++ DirectoryService/src/DirectoryService.Domain/DepartmentLocation.cs
@@ -0,0 +1,26 @@
+﻿namespace DirectoryService.Domain;
+
+public class DepartmentLocation
+{
+    public DepartmentLocation() { }
+    
+    public DepartmentLocation() { }
+    
+    public Guid Id { get; private set; }
+    
+    public Guid LocationId { get; private set; }
+    
+    public Guid DepartmentId { get; private set; }
+    
+    public Location Location { get; private set; }
+    
+    public Depatment Depatment { get; private set; }
+}
+
+public class Depatment
+{
+}
+
+public class Location
+{
+}
