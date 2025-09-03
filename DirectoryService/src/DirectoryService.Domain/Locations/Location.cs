d80ebd6a1045a0b76b18491987b9705072dbd24f
FrMks <maxfrancuzov@gmail.com>
2025-09-03 20:40:04 +0300

    feat: add Name value object

---
+++ DirectoryService/src/DirectoryService.Domain/Locations/Location.cs
@@ -0,0 +1,18 @@
+﻿using DirectoryService.Domain.Locations;
+
+namespace DirectoryService.Domain;
+
+public class Location
+{
+    private Location() { }
+
+    private Location(Guid id, Name name)
+    {
+        Id = id;
+        Name = name;
+    }
+
+    public Guid Id { get; private set; }
+    
+    public Name Name { get; private set; }
+}
