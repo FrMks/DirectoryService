2c4bf8b6c3cbdba557413cea9b9f02b0ae4dd93c
FrMks <maxfrancuzov@gmail.com>
2025-09-03 20:20:38 +0300

    feat: filled the DepartmentLocation.cs class with constructors, properties, methods for creating class instances and methods for setting property values

--- DirectoryService/src/DirectoryService.Domain/DepartmentLocation.cs
+++ DirectoryService/src/DirectoryService.Domain/DepartmentLocation.cs
@@ -1,6 +1,48 @@
-﻿namespace DirectoryService.Domain;
+﻿using System.Dynamic;
+using CSharpFunctionalExtensions;
+
+namespace DirectoryService.Domain;
 
 public class DepartmentLocation
 {
+    private DepartmentLocation() { }
+
+    private DepartmentLocation(Guid id, Guid departmentId,
+        Guid locationId, Location location, Department department)
+    {
+        Id = id;
+        DepartmentId = departmentId;
+        LocationId = locationId;
+        Location = location;
+        Department = department;
+    }
+
     public Guid Id { get; private set; }
+
+    public Guid DepartmentId { get; private set; }
+
+    public Guid LocationId { get; private set; }
+
+    public Location Location { get; private set; }
+
+    public Department Department { get; private set; }
+
+    public static Result<DepartmentLocation> Create(Guid id, Guid departmentId,
+        Guid locationId, Location location, Department department)
+    {
+        DepartmentLocation departmentLocation = new DepartmentLocation(
+            id, departmentId, locationId, location, department);
+
+        return Result.Success(departmentLocation);
+    }
+
+    public void SetId(Guid id) => Id = id;
+
+    public void SetDepartmentId(Guid departmentId) => DepartmentId = departmentId;
+
+    public void SetLocationId(Guid locationId) => LocationId = locationId;
+
+    public void SetLocation(Location location) => Location = location;
+
+    public void SetDepartment(Department department) => Department = department;
 }
