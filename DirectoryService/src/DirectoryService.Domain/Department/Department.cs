71ff62277fe7fa3c9871fdef3e2d42d5823c03b1
FrMks <maxfrancuzov@gmail.com>
2025-09-04 19:55:59 +0300

    add parentId property to Department.cs

--- DirectoryService/src/DirectoryService.Domain/Department/Department.cs
+++ DirectoryService/src/DirectoryService.Domain/Department/Department.cs
@@ -17,4 +17,17 @@ public class Department
     public Name Name { get; private set; }
     
     public Identifier Identifier { get; private set; }
+    
+    /// <summary>
+    /// Компания "Рога и копыта"
+    /// ├── IT отдел (ParentId = null - это корень)
+    ///     │   ├── Backend команда (ParentId = ID of "IT отдел")
+    /// │   └── Frontend команда (ParentId = ID of "IT отдел")
+    /// ├── HR отдел (ParentId = null - это тоже корень)
+    ///     │   ├── Найм (ParentId = ID of "HR отдел")
+    /// │   └── Обучение (ParentId = ID of "HR отдел")
+    /// └── Продажи (ParentId = null - корень)
+    ///     └── B2B продажи (ParentId = ID of "Продажи")
+    /// </summary>
+    public Guid? ParentId { get; private set; }
 }
