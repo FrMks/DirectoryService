628a15f6e7865af899a593419d8f1c12e914e98e
FrMks <maxfrancuzov@gmail.com>
2025-09-04 19:30:30 +0300

    feat: create and use Identifier value object

--- DirectoryService/src/DirectoryService.Domain/Department/Department.cs
+++ DirectoryService/src/DirectoryService.Domain/Department/Department.cs
@@ -1,16 +1,20 @@
-﻿using DirectoryService.Domain.Department.ValueObject;
+﻿using CSharpFunctionalExtensions;
+using DirectoryService.Domain.Department.ValueObject;
 
 namespace DirectoryService.Domain.Department;
 
 public class Department
 {
-    private Department(Guid id, Name name)
+    private Department(Name name, Identifier identifier)
     {
-        Id = id;
+        Id = Guid.NewGuid();
         Name = name;
+        Identifier = identifier;
     }
     
     public Guid Id { get; private set; }
     
     public Name Name { get; private set; }
+    
+    public Identifier Identifier { get; private set; }
 }
---
+++ DirectoryService/src/DirectoryService.Domain/Department/ValueObject/Identifier.cs
@@ -0,0 +1,41 @@
+﻿using System.Security;
+using System.Text.RegularExpressions;
+using CSharpFunctionalExtensions;
+
+namespace DirectoryService.Domain.Department.ValueObject;
+
+public partial class Identifier
+{
+
+    private Identifier(string identifier)
+    {
+        Value = identifier;
+    }
+    
+    public string Value { get; private set; }
+
+    public static Result<Identifier> Create(string identifier)
+    {
+        if (string.IsNullOrWhiteSpace(identifier))
+            Result.Failure("Identifier is required.");
+        
+        string trimmedIdentifier = identifier.Trim();
+        
+        if (trimmedIdentifier == string.Empty)
+            Result.Failure<Department>("Identifier is required.");
+        
+        if (trimmedIdentifier.Length < 3 || trimmedIdentifier.Length > 150)
+            Result.Failure<Department>("Identifier is invalid.");
+        
+        if (!LatinLettersOnlyRegex().IsMatch(trimmedIdentifier))
+            Result.Failure<Department>("Identifier is invalid.");
+        
+        Identifier instance = new Identifier(identifier);
+        
+        return Result.Success(instance);
+    }
+
+    [GeneratedRegex(@"^[a-zA-Z]+$")]
+    private static partial Regex LatinLettersOnlyRegex();
+
+}
