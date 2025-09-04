8d29682d44fea836792dbb749b643398794cacbb
FrMks <maxfrancuzov@gmail.com>
2025-09-04 19:01:38 +0300

    feat: add timezone value object

--- DirectoryService/src/DirectoryService.Domain/Locations/Location.cs
+++ DirectoryService/src/DirectoryService.Domain/Locations/Location.cs
@@ -4,16 +4,20 @@ namespace DirectoryService.Domain.Locations;
 
 public class Location
 {
-    private Location(Guid id, Name name, Address address)
+    private Location(Guid id, Name name, Address address,
+        Timezone timezone)
     {
         Id = id;
         Name = name;
         Address = address;
+        Timezone = timezone;
     }
 
     public Guid Id { get; private set; }
 
     public Name Name { get; private set; }
-    
+
     public Address Address { get; private set; }
+    
+    public Timezone Timezone { get; private set; }
 }
