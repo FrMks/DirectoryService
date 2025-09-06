d81e3c51ec45b1a5511b2ff1afcb7d3ea08f1b4b
FrMks <maxfrancuzov@gmail.com>
2025-09-06 08:49:24 +0300

    feat: add Name value object to Position

---
+++ DirectoryService/src/DirectoryService.Domain/Positions/Name.cs
@@ -0,0 +1,33 @@
+﻿using CSharpFunctionalExtensions;
+
+namespace DirectoryService.Domain.Positions;
+
+// TODO: написано сделать уникальным, но на сколько я понимаю,
+// то уникальность проверяется по сравнению с чем-то
+// (хотя бы есть массив, в котором лежат другие имена позиций)
+public class Name
+{
+    private Name(string value)
+    {
+        Value = value;
+    }
+    
+    public string Value { get; private set; }
+    
+    public void SetValue(string value) => Value = value;
+
+    public Result<Name> Create(string value)
+    {
+        if (string.IsNullOrEmpty(value))
+            Result.Failure("Name cannot be null or empty");
+            
+        string trimmedValue = value.Trim();
+        
+        if (trimmedValue.Length < 3 || trimmedValue.Length > 100)
+            return Result.Failure<Name>("Name cannot be less than 3 characters and more than 100 characters");
+        
+        Name name = new(trimmedValue);
+
+        return Result.Success(name);
+    }
+}
