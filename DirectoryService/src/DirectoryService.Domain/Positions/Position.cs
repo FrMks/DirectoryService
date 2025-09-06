8b2de96d4619db663c410e1b66184d5360be07ad
FrMks <maxfrancuzov@gmail.com>
2025-09-06 09:03:35 +0300

    feat: add description value object and departmentPositions to Position.cs

--- DirectoryService/src/DirectoryService.Domain/Positions/Position.cs
+++ DirectoryService/src/DirectoryService.Domain/Positions/Position.cs
@@ -5,30 +5,42 @@ namespace DirectoryService.Domain.Positions;
 
 public class Position
 {
-    private Position(Guid id, Name name)
+    private Position(Guid id, Name name, Description description,
+        IReadOnlyList<DepartmentPosition> departmentPositions)
     {
         Id = id;
         Name = name;
+        Description = description;
+        
+        DepartmentPositions = departmentPositions;
     }
 
     #region Properties
 
     public Guid Id { get; private set; }
     public Name Name { get; private set; }
+    public Description Description { get; private set; }
+    
+    public IReadOnlyList<DepartmentPosition> DepartmentPositions { get; private set; }
 
     #endregion
 
     #region Public methods
 
-    public Result<Position> Create(Guid id, Name name)
+    public Result<Position> Create(Guid id, Name name, Description description,
+        IReadOnlyList<DepartmentPosition> departmentPositions)
     {
-        Position position = new(id, name);
+        Position position = new(id, name, description,
+            departmentPositions);
         
         return Result.Success(position);
     }
     
     public void SetId(Guid id) => Id = id;
     public void SetName(Name name) => Name = name;
+    public void SetDescription(Description description) => Description = description;
+    
+    public void SetDepartmentPositions(IReadOnlyList<DepartmentPosition> departmentPositions) => DepartmentPositions = departmentPositions;
 
     #endregion
 }
