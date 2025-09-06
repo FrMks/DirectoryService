2600f53394102dfb06f0a1ca3fa89a4d72ffec52
FrMks <maxfrancuzov@gmail.com>
2025-09-06 08:39:43 +0300

    feat: add full DepartmentPosition.cs and base Position.cs only with id

---
+++ DirectoryService/src/DirectoryService.Domain/Positions/Position.cs
@@ -0,0 +1,13 @@
+﻿namespace DirectoryService.Domain.Positions;
+
+public class Position
+{
+    private Position(Guid id)
+    {
+        Id = id;
+    }
+    
+    public Guid Id { get; private set; }
+    
+    public void SetId(Guid id) => Id = id;
+}
