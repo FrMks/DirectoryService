2fbdf4da0f75d5d4dae021c251d56a7d142155fb
FrMks <maxfrancuzov@gmail.com>
2025-09-09 20:23:56 +0300

    feat: add length limit to properties in Address.cs, add constants for length limits

---
+++ DirectoryService/src/DirectoryService.Domain/LengthConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace DirectoryService.Domain;
+
+public readonly struct LengthConstants
+{
+    public const int LENGTH120 = 120;
+    public const int LENGTH3 = 3;
+    public const int LENGTH100 = 100;
+    public const int LENGTH60 = 60;
+}
--- DirectoryService/src/DirectoryService.Domain/Locations/ValueObjects/Address.cs
+++ DirectoryService/src/DirectoryService.Domain/Locations/ValueObjects/Address.cs
@@ -19,18 +19,53 @@ public record Address
 
     public static Result<Address> Create(string street, string city, string country)
     {
+        #region street
+
         if (string.IsNullOrWhiteSpace(street))
             return Result.Failure<Address>("Street cannot be empty");
         
+        string trimmedStreet = street.Trim();
+        
+        if (string.IsNullOrWhiteSpace(trimmedStreet))
+            return Result.Failure<Address>("Street cannot be empty");
+        
+        if (trimmedStreet.Length > LengthConstants.LENGTH100)
+            return Result.Failure<Address>("Street cannot be more than 100 characters");
+
+        #endregion
+
+        #region city
+
         if (string.IsNullOrWhiteSpace(city))
             return Result.Failure<Address>("City cannot be empty");
         
+        string trimmedCity = city.Trim();
+        
+        if (string.IsNullOrWhiteSpace(trimmedCity))
+            return Result.Failure<Address>("City cannot be empty");
+        
+        if (trimmedCity.Length > LengthConstants.LENGTH60)
+            return Result.Failure<Address>("City cannot be more than 60 characters");
+
+        #endregion
+
+        #region country
+
         if (string.IsNullOrWhiteSpace(country))
             return Result.Failure<Address>("Country cannot be empty");
+        
+        string trimmedCountry = country.Trim();
+        
+        if (string.IsNullOrWhiteSpace(trimmedCountry))
+            return Result.Failure<Address>("Country cannot be empty");
+        
+        if (trimmedCountry.Length > LengthConstants.LENGTH60)
+            return Result.Failure<Address>("Country cannot be more than 60 characters");
+
+        #endregion
         
         Address address = new(street, city, country);
         
         return Result.Success(address);
     }
-
 }
