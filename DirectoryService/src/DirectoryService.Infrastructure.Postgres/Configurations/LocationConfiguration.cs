6bf50f7fd6b27c2f6678c3b7fb31fd0d3a4adf30
FrMks <maxfrancuzov@gmail.com>
2025-09-09 20:28:58 +0300

    feat: create DirectoryServiceDbContext.cs class and add LocationConfiguration.cs and working with id property and address value object

---
+++ DirectoryService/src/DirectoryService.Infrastructure.Postgres/Configurations/LocationConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using DirectoryService.Domain;
+using DirectoryService.Domain.Locations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DirectoryService.Infrastructure.Postgres.Configurations;
+
+public class LocationConfiguration : IEntityTypeConfiguration<Location> 
+{
+    public void Configure(EntityTypeBuilder<Location> builder)
+    {
+        builder.ToTable("locations");
+        
+        builder.HasKey(l => l.Id).HasName("id");
+
+        // builder.OwnsOne(l => l.Address, addressBuilder =>
+        builder.ComplexProperty(l => l.Address, addressBuilder =>
+        {
+            addressBuilder
+                .Property(l => l.Street)
+                .IsRequired()
+                .HasMaxLength(LengthConstants.LENGTH100)
+                .HasColumnName("street");
+            
+            addressBuilder
+                .Property(l => l.City)
+                .IsRequired()
+                .HasMaxLength(LengthConstants.LENGTH60)
+                .HasColumnName("city");
+            
+            addressBuilder
+                .Property(l => l.Country)
+                .IsRequired()
+                .HasMaxLength(LengthConstants.LENGTH60)
+                .HasColumnName("country");
+        });
+        // Если нужно, чтобы был nullable VO, то:
+        // builder.Navigation(l => l.Address).IsRequired(false);
+    }
+}
---
+++ DirectoryService/src/DirectoryService.Infrastructure.Postgres/DirectoryServiceDbContext.cs
@@ -0,0 +1,26 @@
+﻿using DirectoryService.Domain.Locations;
+using Microsoft.EntityFrameworkCore;
+
+namespace DirectoryService.Infrastructure.Postgres;
+
+public class DirectoryServiceDbContext : DbContext
+{
+    private readonly string _connectionString;
+
+    public DirectoryServiceDbContext(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(_connectionString); // Используй Npgsql в качестве бд
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        
+    }
+
+    public DbSet<Location> Locations => Set<Location>();
+}
