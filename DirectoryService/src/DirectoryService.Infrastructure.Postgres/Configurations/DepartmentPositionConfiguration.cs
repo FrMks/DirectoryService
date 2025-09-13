7c37b444759ca142356a1519741ec0b22a4362f1
FrMks <maxfrancuzov@gmail.com>
2025-09-13 16:47:42 +0300

    feat: DepartmentPositionConfiguration.cs add id, departmentId, positionId properties

---
+++ DirectoryService/src/DirectoryService.Infrastructure.Postgres/Configurations/DepartmentPositionConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using DirectoryService.Domain;
+using DirectoryService.Domain.Department;
+using DirectoryService.Domain.Locations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DirectoryService.Infrastructure.Postgres.Configurations;
+
+public class DepartmentPositionConfiguration : IEntityTypeConfiguration<DepartmentPosition>
+{
+    public void Configure(EntityTypeBuilder<DepartmentPosition> builder)
+    {
+        builder.ToTable("department_positions");
+        
+        builder.HasKey(dp => dp.Id).HasName("pk_department_positions");
+        
+        builder.Property(dp => dp.Id)
+            .HasColumnName("id");
+        
+        builder.Property(dp => dp.DepartmentId)
+            .HasColumnName("department_id")
+            .IsRequired();
+        
+        builder.Property(dp => dp.PositionId)
+            .HasColumnName("position_id")
+            .IsRequired();
+        
+        builder.HasOne<Department>()
+            .WithMany()
+            .HasForeignKey(dp => dp.DepartmentId)
+            .OnDelete(DeleteBehavior.Cascade);
+        
+        builder.HasOne<Location>()
+            .WithMany()
+            .HasForeignKey(dp => dp.PositionId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
