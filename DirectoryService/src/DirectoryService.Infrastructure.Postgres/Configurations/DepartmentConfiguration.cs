be1948838cde6c017ed70122d6d4fb8218040434
FrMks <maxfrancuzov@gmail.com>
2025-09-13 15:50:33 +0300

    feat: add DepartmentConfiguration.cs with Id property

---
+++ DirectoryService/src/DirectoryService.Infrastructure.Postgres/Configurations/DepartmentConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using DirectoryService.Domain.Department;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DirectoryService.Infrastructure.Postgres.Configurations;
+
+public class DepartmentConfiguration : IEntityTypeConfiguration<Department>
+{
+    public void Configure(EntityTypeBuilder<Department> builder)
+    {
+        builder.HasKey(d => d.Id).HasName("pk_department");
+        
+        builder.Property(d => d.Id).HasColumnName("department_id");
+    }
+}
