b66a8e1f55758f5ca411b9fa3861d8b455a584c5
FrMks <maxfrancuzov@gmail.com>
2025-09-13 15:59:12 +0300

    feat: DepartmentConfiguration.cs add Name and Identifier properties

--- DirectoryService/src/DirectoryService.Infrastructure.Postgres/Configurations/DepartmentConfiguration.cs
+++ DirectoryService/src/DirectoryService.Infrastructure.Postgres/Configurations/DepartmentConfiguration.cs
@@ -1,6 +1,8 @@
 ﻿using DirectoryService.Domain.Department;
+using DirectoryService.Domain.Department.ValueObject;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Name = DirectoryService.Domain.Department.ValueObject.Name;
 
 namespace DirectoryService.Infrastructure.Postgres.Configurations;
 
@@ -11,5 +13,13 @@ public class DepartmentConfiguration : IEntityTypeConfiguration<Department>
         builder.HasKey(d => d.Id).HasName("pk_department");
         
         builder.Property(d => d.Id).HasColumnName("department_id");
+
+        builder.Property(d => d.Name)
+            .HasConversion(d => d.Value, name => Name.Create(name).Value)
+            .HasColumnType("name");
+        
+        builder.Property(d => d.Identifier)
+            .HasConversion(d => d.Value, identifier => Identifier.Create(identifier).Value)
+            .HasColumnType("identifier");
     }
 }
