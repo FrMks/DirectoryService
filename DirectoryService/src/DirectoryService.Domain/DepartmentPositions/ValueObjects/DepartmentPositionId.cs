72eb0632c5c4b4dea8dcbbe337375a6baccbf1e2
FrMks <maxfrancuzov@gmail.com>
2025-09-15 18:38:10 +0300

    feat: DepartmentPositionId.cs add DepartmentPositionId Value object

---
+++ DirectoryService/src/DirectoryService.Domain/DepartmentPositions/ValueObjects/DepartmentPositionId.cs
@@ -0,0 +1,15 @@
+﻿namespace DirectoryService.Domain.DepartmentPositions.ValueObjects;
+
+public record DepartmentPositionId
+{
+    private DepartmentPositionId(Guid value)
+    {
+        Value = value;
+    }
+    
+    public Guid Value { get; }
+    
+    public static DepartmentPositionId NewDepartmentId() => new (Guid.NewGuid());
+    
+    public static DepartmentPositionId Empty() => new (Guid.Empty);
+}
