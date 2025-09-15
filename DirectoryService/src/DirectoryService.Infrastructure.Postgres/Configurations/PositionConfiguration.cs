26536841fbc445ccf14770d84d238f04958d7b37
FrMks <maxfrancuzov@gmail.com>
2025-09-15 18:13:09 +0300

    feat: PositionConfiguration.cs create properties in infrastructure

---
+++ DirectoryService/src/DirectoryService.Infrastructure.Postgres/Configurations/PositionConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using DirectoryService.Domain.Positions;
+using DirectoryService.Domain.Positions.ValueObject;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DirectoryService.Infrastructure.Postgres.Configurations;
+
+public class PositionConfiguration : IEntityTypeConfiguration<Position>
+{
+    public void Configure(EntityTypeBuilder<Position> builder)
+    {
+        builder.ToTable("positions");
+        
+        builder.HasKey(p => p.Id).HasName("pk_positions");
+
+        builder.Property(p => p.Id)
+            .HasColumnName("id");
+
+        builder.Property(p => p.Name)
+            .HasConversion(p => p.Value, name => Name.Create(name).Value)
+            .HasColumnName("name");
+
+        builder.Property(p => p.Description)
+            .HasConversion(p => p.Value, description => Description.Create(description).Value)
+            .HasColumnName("description");
+
+        builder.Property(p => p.IsActive)
+            .HasColumnName("is_active");
+        
+        builder.Property(p => p.CreatedAt)
+            .HasColumnName("created_at");
+        
+        builder.Property(p => p.UpdateAt)
+            .HasColumnName("update_at");
+
+        builder.HasMany(p => p.DepartmentPositions)
+            .WithOne()
+            .HasForeignKey(p => p.DepartmentId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
