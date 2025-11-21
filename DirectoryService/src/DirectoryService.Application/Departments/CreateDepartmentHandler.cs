c0abe9291c8cd2413ba07b5082ae9c3502c744b4
FrMks <maxfrancuzov@gmail.com>
2025-11-21 12:41:24 +0300

    feat: controllers and handler. handler need to refactor

--- DirectoryService/src/DirectoryService.Application/Departments/CreateDepartmentHandler.cs
+++ DirectoryService/src/DirectoryService.Application/Departments/CreateDepartmentHandler.cs
@@ -3,9 +3,13 @@ using DirectoryService.Application.Abstractions;
 using DirectoryService.Application.Departments.Interfaces;
 using DirectoryService.Application.Extensions;
 using DirectoryService.Contracts.Departments;
+using DirectoryService.Domain;
+using DirectoryService.Domain.Department;
+using DirectoryService.Domain.Department.ValueObject;
 using FluentValidation;
 using Microsoft.Extensions.Logging;
 using Shared;
+using Path = DirectoryService.Domain.Department.ValueObject.Path;
 
 namespace DirectoryService.Application.Departments;
 
@@ -28,5 +32,74 @@ public class CreateDepartmentHandler(
 
             return validationResult.ToList();
         }
+        
+        // Создание сущности Department
+        DepartmentId departmentId = DepartmentId.NewDepartmentId();
+        
+        var departmentNameResult = Name.Create(departmentCommand.DepartmentRequest.Name);
+        Name departmentName = departmentNameResult.Value;
+        
+        var departmentIdentifierResult = Identifier.Create(departmentCommand.DepartmentRequest.Identifier);
+        var departmentIdentifier = departmentIdentifierResult.Value;
+
+        var departmentParentIdResult = departmentCommand.DepartmentRequest.ParentId;
+
+        Path departmentPath;
+        Depth departmentDepth;
+        Department? parentDepartment = null;
+        if (departmentParentIdResult == null)
+        {
+            departmentPath = Path.CreateParent(departmentIdentifier);
+            var departmentDepthResult = Depth.Create(0);
+            departmentDepth = departmentDepthResult.Value;
+        }
+        else
+        {
+            var parentDepartmentResult = await departmentsRepository
+                .GetByIdAsync(DepartmentId.FromValue(departmentParentIdResult.Value), cancellationToken);
+
+            if (parentDepartmentResult.IsFailure)
+            {
+                logger.LogInformation("{parentDepartmentResult.Error}", parentDepartmentResult.Error);
+                return parentDepartmentResult.Error;
+            }
+            
+            parentDepartment = parentDepartmentResult.Value;
+
+            departmentPath = parentDepartment.Path.CreateChild(departmentIdentifier);
+            short depthCount = (short)(parentDepartment.Depth.Value + 1);
+            var depthResult = Depth.Create(depthCount);
+
+            if (depthResult.IsFailure)
+            {
+                logger.LogInformation(depthResult.Error.Message);
+                return depthResult.Error.ToErrors();
+            }
+            
+            departmentDepth = depthResult.Value;
+        }
+
+        var department = Department.Create(
+            departmentId,
+            departmentName,
+            departmentIdentifier,
+            departmentPath,
+            new List<DepartmentLocation>(),
+            new List<DepartmentPosition>(),
+            departmentDepth,
+            departmentParentIdResult.Value).Value;
+        
+        logger.LogInformation("Creating department with id {id}", department.Id.Value);
+        
+        // Сохранение сущность Department в БД
+        var successfulId = await departmentsRepository.AddAsync(department, cancellationToken);
+        
+        if (successfulId.IsFailure)
+            return Error.Failure(null, successfulId.Error.Message).ToErrors();
+        
+        // Логирование об успешном или неуспешном сохранении
+        logger.LogInformation("Department with id {successfulId.Value} add to db.", successfulId.Value);
+        
+        return successfulId.Value;
     }
 }
--- DirectoryService/src/DirectoryService.Presentation/Controllers/Departments.cs
+++ DirectoryService/src/DirectoryService.Presentation/Controllers/Departments.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DirectoryService.Application.Abstractions;
+using DirectoryService.Application.Departments;
+using DirectoryService.Contracts.Departments;
+using Microsoft.AspNetCore.Mvc;
 using Shared.EndpointResults;
 
 namespace DirectoryService.Web.Controllers;
@@ -7,9 +10,19 @@ namespace DirectoryService.Web.Controllers;
 [Route("api/departments")]
 public class Departments : ControllerBase
 {
-    // [HttpPost]
-    // public async Task<EndpointResult<Guid>> Create()
-    // {
-    //     
-    // }
+    [HttpPost]
+    public async Task<EndpointResult<Guid>> Create(
+        [FromServices] ICommandHandler<Guid, CreateDepartmentCommand> handler,
+        [FromBody] CreateDepartmentRequest request,
+        CancellationToken cancellationToken)
+    {
+        CreateDepartmentCommand departmentCommand = new(request);
+        
+        var result = await handler.Handle(departmentCommand, cancellationToken);
+        
+        if (result.IsFailure)
+            return result.ConvertFailure<Guid>();
+        
+        return result;
+    }
 }
