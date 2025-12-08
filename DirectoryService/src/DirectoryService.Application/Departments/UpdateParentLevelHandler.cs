2f1a97fcb40ebecedc20610738c86e855f26b461
FrMks <maxfrancuzov@gmail.com>
2025-12-08 16:28:44 +0300

    feat: check that parent id and actual id are not equal to each other and actual department is existed

--- DirectoryService/src/DirectoryService.Application/Departments/UpdateParentLevelHandler.cs
+++ DirectoryService/src/DirectoryService.Application/Departments/UpdateParentLevelHandler.cs
@@ -28,6 +28,37 @@ public class UpdateParentLevelHandler(
             return validationResult.ToList();
         }
 
+        // Проверили, что id места куда хотим поместить не совпадает с тем что хотим перенести
+        if (command.DepartmentId == command.ParentLevelRequest.ParentDepartmentId)
+        {
+            var error = Error.Validation(
+                "department.id.equals.level.id",
+                $"department id: {command.DepartmentId} and level id equal to each other");
+            logger.LogInformation("Error when updating parent level, error: {error}", error.Message);
+            return error.ToErrors();
+        }
+        
+        // Проверили, что id department, который мы хотим переместить на другое место существует
+        DepartmentId departmentId = DepartmentId.FromValue(command.DepartmentId);
+        // TODO: должен ли быть активным id департамента, который мы хотим перенести?
+        var actualDepartmentResult = await departmentsRepository.ExistAndActiveAsync(departmentId, cancellationToken);
+        if (actualDepartmentResult.IsFailure)
+        {
+            logger.LogInformation(
+                "Error when try get by id actual department, error: {error}",
+                actualDepartmentResult.Error);
+            return actualDepartmentResult.Error;
+        }
+
+        if (command.ParentLevelRequest.ParentDepartmentId == null)
+        {
+            // Ставим как родителя
+        }
+        else
+        {
+            // Переносим под родителя с всеми его детьми
+        }
+
         return Result.Success<Guid, Errors>(Guid.NewGuid());
     }
 }
