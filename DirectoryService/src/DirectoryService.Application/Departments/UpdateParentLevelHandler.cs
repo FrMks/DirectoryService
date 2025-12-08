c21df7ab459598100c47a48d62ddf93b6f69086f
FrMks <maxfrancuzov@gmail.com>
2025-12-08 15:57:42 +0300

    feat: use parent level validator in handler

--- DirectoryService/src/DirectoryService.Application/Departments/UpdateParentLevelHandler.cs
+++ DirectoryService/src/DirectoryService.Application/Departments/UpdateParentLevelHandler.cs
@@ -2,6 +2,7 @@
 using DirectoryService.Application.Abstractions;
 using DirectoryService.Application.Departments;
 using DirectoryService.Application.Departments.Interfaces;
+using DirectoryService.Application.Extensions;
 using DirectoryService.Contracts.Departments;
 using FluentValidation;
 using Microsoft.Extensions.Logging;
@@ -15,6 +16,20 @@ public class UpdateParentLevelHandler(
 {
     public async Task<Result<Guid, Errors>> Handle(UpdateParentLevelCommand command, CancellationToken cancellationToken)
     {
+        // Валидация DTO
+        var validationResult = await validator.ValidateAsync(command.ParentLevelRequest, cancellationToken);
+        if (!validationResult.IsValid)
+        {
+            foreach (Error error in validationResult.ToList())
+            {
+                logger.LogInformation("Error when updating parent level, error: {error}", error.Message);
+            }
+
+            return validationResult.ToList();
+        }
+
+        
+
         return Result.Success<Guid, Errors>(Guid.NewGuid());
     }
 }
