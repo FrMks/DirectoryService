024ff7d2e65e5169423e76a1653999498aa64b4e
FrMks <maxfrancuzov@gmail.com>
2025-12-16 13:49:47 +0300

    feat: made the class inherit from IClassFixture to start container asynchronously for working with the database in tests

--- DirectoryService/tests/DirectoryService.IntegrationTests/DirectoryTestWebFactory.cs
+++ DirectoryService/tests/DirectoryService.IntegrationTests/DirectoryTestWebFactory.cs
@@ -4,7 +4,7 @@ using Program = DirectoryService.Presentation.Program;
 
 namespace DirectoryService.IntegrationTests;
 
-public class DirectoryTestWebFactory : WebApplicationFactory<Program>
+public class DirectoryTestWebFactory : WebApplicationFactory<Program>, IAsyncLifetime
 {
     // Запускаем один Docker контейнер с нашей БД для тестов
     private readonly PostgreSqlContainer _dbContainer = new PostgreSqlBuilder()
@@ -14,8 +14,20 @@ public class DirectoryTestWebFactory : WebApplicationFactory<Program>
         .WithPassword("postgres")
         .Build();
 
-    public DirectoryTestWebFactory()
+    // Реализую методы из интерфейса IAsyncLifetime,
+    // чтобы асинхронно выполнять какие-то методы для инициализации класса
+    // и для его уничтожения
+    // ЭТО НУЖНО ЧТОБЫ НЕ ВЫЗЫВАТЬ АСИНХРОННЫЕ МЕТОДЫ ВНУТРИ КОНСТРУКТОРА
+    // Т.К. В КОНСТРУКТОРЕ НЕЛЬЗЯ ВЫЗЫВАТЬ АСИНХРОННЫЕ МЕТОДЫ
+    public async Task InitializeAsync()
     {
-        // _dbContainer.StartAsync()
+        await _dbContainer.StartAsync();
+    }
+
+    // Добавили new, чтобы сказать, чтобы именно этот вызывался для dispose этого класса
+    public new async Task DisposeAsync()
+    {
+        await _dbContainer.StopAsync();
+        await _dbContainer.DisposeAsync();
     }
 }
