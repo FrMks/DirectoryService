5488f44c00ee9837cca40dd5cbb8efdf014e0fc9
FrMks <maxfrancuzov@gmail.com>
2025-12-22 13:27:20 +0300

    refactor: fix namespace

--- DirectoryService/src/DirectoryService.Application/DependencyInjection.cs
+++ DirectoryService/src/DirectoryService.Application/DependencyInjection.cs
@@ -1,10 +1,10 @@
 ﻿using DirectoryService.Application.Abstractions;
-using DirectoryService.Application.Departments;
 using DirectoryService.Application.Departments.Validation;
 using DirectoryService.Application.Locations.Validation;
 using DirectoryService.Application.Positions.Validation;
 using DirectoryService.Contracts.Departments;
 using DirectoryService.Contracts.Locations;
+using DirectoryService.Contracts.Locations.GetLocations;
 using DirectoryService.Contracts.Positions;
 using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
