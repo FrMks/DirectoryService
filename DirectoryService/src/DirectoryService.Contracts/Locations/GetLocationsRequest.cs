41e67b2a240d9b3d6ff9019464de93f054bf4950
FrMks <maxfrancuzov@gmail.com>
2025-12-22 12:51:30 +0300

    feat: add properties to GetLocationsRequest

--- DirectoryService/src/DirectoryService.Contracts/Locations/GetLocationsRequest.cs
+++ DirectoryService/src/DirectoryService.Contracts/Locations/GetLocationsRequest.cs
@@ -1,6 +1,8 @@
 ﻿namespace DirectoryService.Contracts.Locations;
 
-public class GetLocationsRequest
-{
-    
-}
+public record GetLocationsRequest(
+    List<Guid?> DepartmentIds = null, // Если список указан, возвращаются только локации связанные с этим подразделением 
+    string? Search = null, // Поиск по названию локации (частичное совпадение без регистра)
+    bool? IsActive = null, // Фильтр по активности: true - только активные, false - только неактивные
+    int? Page = 1, // Номер страницы (по умолчанию 1)
+    int? PageSize = 20); // Количество элементов по странице (по умолчанию 20)
