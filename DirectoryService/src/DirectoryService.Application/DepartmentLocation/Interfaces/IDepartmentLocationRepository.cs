fc02ac1ea44c5c222c59f0b168093684777c4c14
FrMks <maxfrancuzov@gmail.com>
2025-12-22 14:02:45 +0300

    feat: get locationIds by departmentIds from DepartmentLocation data table

--- DirectoryService/src/DirectoryService.Application/DepartmentLocation/Interfaces/IDepartmentLocationRepository.cs
+++ DirectoryService/src/DirectoryService.Application/DepartmentLocation/Interfaces/IDepartmentLocationRepository.cs
@@ -1,6 +1,10 @@
-﻿namespace DirectoryService.Application.DepartmentLocation.Interfaces;
+﻿using CSharpFunctionalExtensions;
+using DirectoryService.Domain.Locations.ValueObjects;
+using Shared;
+
+namespace DirectoryService.Application.DepartmentLocation.Interfaces;
 
 public interface IDepartmentLocationRepository
 {
-    
+    public Task<Result<List<LocationId>, Error>> GetLocationIdsAsync(List<Guid> departmentIds);
 }
--- DirectoryService/src/DirectoryService.Infrastructure.Postgres/Repositories/DepartmentLocationRepository.cs
+++ DirectoryService/src/DirectoryService.Infrastructure.Postgres/Repositories/DepartmentLocationRepository.cs
@@ -1,5 +1,9 @@
-﻿using DirectoryService.Application.DepartmentLocation.Interfaces;
+﻿using CSharpFunctionalExtensions;
+using DirectoryService.Application.DepartmentLocation.Interfaces;
+using DirectoryService.Domain.Locations.ValueObjects;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Shared;
 
 namespace DirectoryService.Infrastructure.Postgres.Repositories;
 
@@ -8,5 +12,32 @@ public class DepartmentLocationRepository(
     ILogger<DepartmentLocationRepository> logger)
     : IDepartmentLocationRepository
 {
-    
+    public async Task<Result<List<LocationId>, Error>> GetLocationIdsAsync(List<Guid> departmentIds)
+    {
+        if (departmentIds.Any() == false)
+        {
+            logger.LogError("DepartmentIds are empty when searching locations by department ids");
+            return Error.Failure(
+                "departmentIds.are.empty", 
+                "departmentIds are empty when searching locations by department ids");   
+        }
+
+        var departmentLocations = await dbContext.DepartmentLocations
+            .Where(d => departmentIds.Contains(d.DepartmentId))
+            .ToListAsync();
+
+        if (departmentLocations.Count != departmentIds.Count)
+        {
+            logger.LogError("DepartmentIds are not the same number of department locations");
+            return Error.Failure("some.departmentIds.dont.have.in.db",
+                "some departmentIds dont have in DepartmentLocations db");
+        }
+        
+        var locationIds = departmentLocations
+            .Select(dl => dl.LocationId)
+            .Distinct() // Без дубликатов
+            .ToList();
+        
+        return locationIds;
+    }
 }
