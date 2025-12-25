8cf9df525bb6778176343eeba3a398b70271a9a1
FrMks <maxfrancuzov@gmail.com>
2025-12-25 11:37:34 +0300

    feat: moved filtering by department Ids to a separate method

--- DirectoryService/src/DirectoryService.Application/Locations/GetLocationsHandler.cs
+++ DirectoryService/src/DirectoryService.Application/Locations/GetLocationsHandler.cs
@@ -1,4 +1,5 @@
-﻿using CSharpFunctionalExtensions;
+﻿using System.Net;
+using CSharpFunctionalExtensions;
 using DirectoryService.Application.DepartmentLocation.Interfaces;
 using DirectoryService.Application.Extensions;
 using DirectoryService.Application.Locations.Interfaces;
@@ -15,7 +16,9 @@ public class GetLocationsHandler(
     IValidator<GetLocationsRequest> validator,
     ILogger<CreateLocationHandler> logger)
 {
-    public async Task<Result<List<GetLocationsResponse>, Errors>> Handle(GetLocationsCommand locationsCommand, CancellationToken cancellationToken)
+    public async Task<Result<List<GetLocationsResponse>, Errors>> Handle(
+        GetLocationsCommand locationsCommand,
+        CancellationToken cancellationToken)
     {
         // Валидация DTO
         var validationResult = await validator.ValidateAsync(locationsCommand.LocationsRequest, cancellationToken);
@@ -29,43 +32,59 @@ public class GetLocationsHandler(
             return validationResult.ToList();
         }
 
+        List<GetLocationsResponse> mappedLocations;
+        
         if (locationsCommand.LocationsRequest.DepartmentIds.Count != 0)
         {
-            var locationIds = await departmentLocationRepository.GetLocationIdsAsync(
-                locationsCommand.LocationsRequest.DepartmentIds,
+            var mappedLocationsResult = await FilterByDepartmentIds(
+                departmentLocationRepository,
+                locationsCommand,
                 cancellationToken);
 
-            if (locationIds.IsFailure)
-                return locationIds.Error.ToErrors();
-
-            var guidLocationIds = locationIds.Value
-                .Select(d => d.Value)
-                .ToList();
+            if (mappedLocationsResult.IsFailure)
+                return mappedLocationsResult.Error.ToErrors();
             
-            var locations = await locationsRepository.GetLocationsAsync(
-                guidLocationIds,
-                cancellationToken);
-            
-            if (locations.IsFailure)
-                return locations.Error.ToErrors();
-
-            var mappedLocations = locations.Value
-                .Select(l => new GetLocationsResponse
-                {
-                    Id = l.Id.Value,
-                    Name = l.Name.Value,
-                    Street = l.Address.Street,
-                    City = l.Address.City,
-                    Country = l.Address.Country,
-                    Timezone = l.Timezone.Value,
-                    IsActive = l.IsActive,
-                    CreatedAt = l.CreatedAt,
-                    UpdatedAt = l.UpdatedAt
-                }).ToList();
-            
-            return mappedLocations;
+            mappedLocations = mappedLocationsResult.Value;
         }
         
         return Result.Success<List<GetLocationsResponse>, Errors>(new List<GetLocationsResponse>());
+    }
+
+    private async Task<Result<List<GetLocationsResponse>, Error>> FilterByDepartmentIds(
+        IDepartmentLocationRepository departmentLocationRepository,
+        GetLocationsCommand locationsCommand,
+        CancellationToken cancellationToken)
+    {
+        var locationIds = await departmentLocationRepository.GetLocationIdsAsync(
+            locationsCommand.LocationsRequest.DepartmentIds,
+            cancellationToken);
+
+        if (locationIds.IsFailure)
+            return locationIds.Error;
+
+        var guidLocationIds = locationIds.Value
+            .Select(d => d.Value)
+            .ToList();
+            
+        var locations = await locationsRepository.GetLocationsAsync(
+            guidLocationIds,
+            cancellationToken);
+            
+        if (locations.IsFailure)
+            return locations.Error;
+
+        return locations.Value
+            .Select(l => new GetLocationsResponse
+            {
+                Id = l.Id.Value,
+                Name = l.Name.Value,
+                Street = l.Address.Street,
+                City = l.Address.City,
+                Country = l.Address.Country,
+                Timezone = l.Timezone.Value,
+                IsActive = l.IsActive,
+                CreatedAt = l.CreatedAt,
+                UpdatedAt = l.UpdatedAt
+            }).ToList();
     }
 }
