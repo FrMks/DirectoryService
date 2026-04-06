e3d72da881b04ca593a7c5f14a16e905a51e1778
FrMks <maxfrancuzov@gmail.com>
2026-04-06 12:33:57 +0300

    feat: implement department cleanup background service with configuration options

---
+++ DirectoryService/src/DirectoryService.Infrastructure.Postgres/DepartmentCleanupBackgroundService/DepartmentCleanupOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace DirectoryService.Infrastructure.Postgres.DepartmentCleanupBackgroundService;
+
+public class DepartmentCleanupOptions
+{
+    public bool Enabled { get; set; } = true;
+    
+    public double IntervalHours { get; set; } = 24;
+
+    public int InactiveDaysThreshold { get; set; } = 30;
+}
