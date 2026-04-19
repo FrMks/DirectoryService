4a241fbe692e2c73d1454552c3301aa648621a38
FrMks <maxfrancuzov@gmail.com>
2026-04-19 14:03:06 +0300

    feat: use button from shudcn

--- client/src/app/counter.tsx
+++ client/src/app/counter.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { JSX } from "react";
 
 export default function Counter(): JSX.Element {
@@ -16,12 +17,7 @@ export default function Counter(): JSX.Element {
   return (
     <div className="flex flex-col gap-4">
       <CoolCount count={count} />
-      <button
-        onClick={handleClick}
-        className="px-4 py-2 bg-blue-500 text-white font-medium rounded shadow-md hover:bg-blue-600 active:bg-blue-700 transition-colors duration-200"
-      >
-        Увеличить
-      </button>
+      <Button onClick={handleClick}>Увеличить</Button>
     </div>
   );
 }
