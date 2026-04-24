b20d1a2652af666b3f1724fdb9925d01abba78dd
FrMks <maxfrancuzov@gmail.com>
2026-04-24 08:20:05 +0300

    refactor: envelope to shared

--- client/src/entities/locations/api.ts
+++ client/src/entities/locations/api.ts
@@ -1,6 +1,7 @@
 import { Location } from "./type";
 import { GetLocationsResponse } from "./getLocationsResponse";
 import { apiClient } from "@/shared/api/axios-instance";
+import { Envelope } from "@/shared/api/envelope";
 
 export type CreateLocationRequest = {
     name: string;
@@ -9,9 +10,14 @@ export type CreateLocationRequest = {
 };
 
 export async function getLocations(): Promise<Location[]> {
-    const response = await apiClient.get<GetLocationsResponse>("/locations");
+    const response = await apiClient.get<Envelope<GetLocationsResponse>>("/locations");
+    const result = response.data.result;
 
-    return response.data.locations.map((location) => ({
+    if (!result) {
+        throw new Error("Locations response does not contain result.");
+    }
+
+    return result.locations.map((location) => ({
         id: location.id,
         name: location.name,
         address: {
