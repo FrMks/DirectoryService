917f2d0a250d0797a8556efccb9e3cb616267c3d
FrMks <maxfrancuzov@gmail.com>
2026-04-30 09:04:02 +0300

    feat: refacotor backend for client (get locations)

--- backend/src/DirectoryService/DirectoryService.Contracts/Locations/GetLocations/GetLocationsResult.cs
+++ backend/src/DirectoryService/DirectoryService.Contracts/Locations/GetLocations/GetLocationsResult.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace DirectoryService.Contracts.Locations.GetLocations;
 
 public record GetLocationsResult(
-    List<GetLocationsResponse> Locations,
-    long TotalCount
+    List<GetLocationsResponse> Items,
+    long TotalCount,
+    int Page,
+    int PageSize,
+    int TotalPages
 );
