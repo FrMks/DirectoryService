1b845125ad088125e0c401fd59a4e3f13642af53
FrMks <maxfrancuzov@gmail.com>
2026-06-22 16:31:42 +0300

    feat: class for integration tests with file service in directory service

---
+++ backend/tests/DirectoryService.IntegrationTests/FileServiceCommunication/FileServiceCommunicationTests.cs
@@ -0,0 +1,13 @@
+﻿namespace DirectoryService.IntegrationTests.FileServiceCommunication;
+
+// Фальшивый File Service
+// ^ Http
+// |
+// IFileCommunicationService
+// ^ DI
+// |
+// DirectoryService.IntegrationTests
+public class FileServiceCommunicationTests
+{
+
+}
