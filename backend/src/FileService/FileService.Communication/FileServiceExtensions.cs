7c3138ff6d8427c6d0e970454271c1d25960f6fc
FrMks <maxfrancuzov@gmail.com>
2026-06-22 14:14:07 +0300

    Add File Service contracts and communication packages

---
+++ backend/src/FileService/FileService.Communication/FileServiceExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace FileService.Communication;
+
+// Чтобы указать: services.AddFilesService(configuration); в другом микросервисе.
+public static class FileServiceExtensions
+{
+    public static IServiceCollection AddFilesServiceHttpCommunication(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.Configure<FileServiceOptions>(configuration.GetSection(nameof(FileServiceOptions)));
+
+        services.AddHttpClient<IFileCommunicationService, FileHttpClient>((serviceProvider, clientConfig) =>
+        {
+            FileServiceOptions fileOptions = serviceProvider.GetRequiredService<IOptions<FileServiceOptions>>().Value;
+
+            clientConfig.BaseAddress = new Uri(fileOptions.Url);
+            clientConfig.Timeout = TimeSpan.FromSeconds(fileOptions.TimeoutSeconds);
+        });
+
+        return services;
+    }
+}
---
+++ backend/src/FileService/FileService.Communication/FileServiceOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace FileService.Communication;
+
+public record FileServiceOptions
+{
+    public const string SectionName = "FileService";
+
+    public string Url { get; init; } = string.Empty;
+
+    public int TimeoutSeconds { get; init; } = 7;
+}
