4be11fd68629956cc55ee7dad6d4411fecb8bd3d
FrMks <maxfrancuzov@gmail.com>
2026-07-22 08:10:40 +0300

    feat: extract metadata step handler finish

--- backend/src/FileService/FileService.VideoProcessing/Pipeline/Steps/ExtractMetadataStepHandler.cs
+++ backend/src/FileService/FileService.VideoProcessing/Pipeline/Steps/ExtractMetadataStepHandler.cs
@@ -1,40 +1,63 @@
 ﻿using CSharpFunctionalExtensions;
+using FileService.Core.Multipart;
+using FileService.Domain.Errors;
 using FileService.Domain.MediaProcessing;
 using FileService.Domain.ValueObjects;
-using Microsoft.AspNetCore.Routing.Internal;
+using FileService.VideoProcessing.FfmpegProcess;
+using Microsoft.Extensions.Logging;
 using Shared;
 
 namespace FileService.VideoProcessing.Pipeline.Steps;
 
 public class ExtractMetadataStepHandler : IProcessingStepHandler
 {
+    private readonly ILogger<ExtractMetadataStepHandler> _logger;
+    private readonly IFfmpegProcessRunner _ffmpegProcessRunner;
+    private readonly IS3Provider _s3Provider;
+
+    public ExtractMetadataStepHandler(
+        ILogger<ExtractMetadataStepHandler> logger,
+        IFfmpegProcessRunner ffmpegProcessRunner,
+        IS3Provider s3Provider)
+    {
+        _logger = logger;
+        _ffmpegProcessRunner = ffmpegProcessRunner;
+        _s3Provider = s3Provider;
+    }
+
     public StepType StepType => StepType.EXTRACT_METADATA;
 
-    public Task<Result<ProcessingContext, Error>> ExecuteAsync(
+    public async Task<Result<ProcessingContext, Error>> ExecuteAsync(
         ProcessingContext context,
         CancellationToken cancellationToken = default)
     {
+        _logger.LogInformation(
+            "Extract metadata for VideoAssetId: {VideoAssetId} start",
+            context.VideoAsset.Id);
 
+        StorageKey? uploadKey = context.VideoAsset.UploadedKey;
+        if (uploadKey is null)
+            return FileError.ObjectNotFound();
 
-        if (cancellationToken.IsCancellationRequested)
-        {
-            var error = Error.Failure("processing.cancelled", "Video processing was cancelled");
-            return Task.FromResult(Result.Failure<ProcessingContext, Error>(error));
-        }
+        Result<string, Error> downloadUrlResult = await _s3Provider
+            .GenerateDownloadUrlAsync(uploadKey);
+        if (downloadUrlResult.IsFailure)
+            return downloadUrlResult.Error;
 
-        Result<VideoMetadata, Error> videoMetadataResult = VideoMetadata.Create(
-            TimeSpan.FromSeconds(120),
-            1920,
-            1080,
-            "h264",
-            "mp4");
-        if (videoMetadataResult.IsFailure)
-            return Task.FromResult(Result.Failure<ProcessingContext, Error>(videoMetadataResult.Error));
+        Result<VideoMetadata, Error> metadataResult = await _ffmpegProcessRunner.ExtractMetadataAsync(
+            downloadUrlResult.Value,
+            cancellationToken);
+        if (metadataResult.IsFailure)
+            return metadataResult.Error;
 
-        UnitResult<Error> setMetadataResult = context.VideoAsset.SetMetadata(videoMetadataResult.Value);
+        UnitResult<Error> setMetadataResult = context.VideoAsset.SetMetadata(metadataResult.Value);
         if (setMetadataResult.IsFailure)
-            return Task.FromResult(Result.Failure<ProcessingContext, Error>(setMetadataResult.Error));
+            return setMetadataResult.Error;
 
-        return Task.FromResult(Result.Success<ProcessingContext, Error>(context));
+        _logger.LogInformation(
+            "Extract metadata for VideoAssetId: {VideoAssetId} finished",
+            context.VideoAsset.Id);
+
+        return context;
     }
 }
