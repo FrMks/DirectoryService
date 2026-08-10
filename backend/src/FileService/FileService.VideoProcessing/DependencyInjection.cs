d9bcc019a40aed9b02fba9ca6be3a0781d567a60
FrMks <maxfrancuzov@gmail.com>
2026-08-10 10:31:24 +0300

    feat: inject IProcessingJobFactory

--- backend/src/FileService/FileService.VideoProcessing/DependencyInjection.cs
+++ backend/src/FileService/FileService.VideoProcessing/DependencyInjection.cs
@@ -1,4 +1,6 @@
-﻿using FileService.VideoProcessing.FfmpegProcess;
+﻿using FileService.Core.Processing;
+using FileService.VideoProcessing.FfmpegProcess;
+using FileService.VideoProcessing.Jobs;
 using FileService.VideoProcessing.Pipeline;
 using FileService.VideoProcessing.Pipeline.CleanupService;
 using FileService.VideoProcessing.Pipeline.Steps;
@@ -12,6 +14,8 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddVideoProcessing(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddScoped<IProcessingJobFactory, VideoProcessingJobFactory>();
+
         services.Configure<VideoProcessingOptions>(
             configuration.GetSection(VideoProcessingOptions.SECTION_NAME));
 
